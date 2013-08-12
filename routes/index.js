750ffade36327121697db273dfbdade5cc1cbe3e
Mike Kamermans <pomax@nihongoresources.com>
2013-08-12 11:02:07 -0700

    stubs for photo, collection and set dedicated pages

--- routes/index.js
+++ routes/index.js
@@ -14,21 +14,33 @@ module.exports = function(app, informationArchitecture) {
      * Set view
      */
     photo: function(req, res) {
-      res.render("photo.html", informationArchitecture);
+      var photos = informationArchitecture.photos,
+          photo = photos[res.locals.photo];
+      informationArchitecture.photo = photo;
+      res.render("dedicated_photo.html", informationArchitecture);
+      delete informationArchitecture.photo;
     },
 
     /**
      * Set view
      */
     set: function(req, res) {
-      res.render("set.html", informationArchitecture);
+      var photosets = informationArchitecture.photosets,
+          photoset = photosets[res.locals.set];
+      informationArchitecture.photoset = photoset;
+      res.render("dedicated_set.html", informationArchitecture);
+      delete informationArchitecture.photoset;
     },
 
     /**
      * Collection view
      */
     collection: function(req, res) {
-      res.render("collection.html", informationArchitecture);
+      var collections = informationArchitecture.collections,
+          collection = collections[res.locals.collection];
+      informationArchitecture.collection = collection;
+      res.render("dedicated_collection.html", informationArchitecture);
+      delete informationArchitecture.collection;
     },
 
     /**
